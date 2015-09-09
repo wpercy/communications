f1774ed91f11994eabf4a91414ce5122f9712f9b
Will Percival <wpercy27@gmail.com>
2015-09-09 12:10:18 -0400

    can get html from a file, can also provide a logfile to get a csv of all the sent emails

--- ses_emailer.py
+++ ses_emailer.py
@@ -1,9 +1,9 @@
 #! /usr/bin/python
+import csv
 import boto3
 import datetime
 
-class ses_mailer:
-
+class SES_mailer:
     def __init__(self, credentials=None):
         if credentials:
             try:
@@ -11,8 +11,7 @@ class ses_mailer:
                 self.aws_access_key_id = credentials['aws_access_key_id'],
                 self.aws_secret_access_key = credentials['aws_secret_access_key']
             except KeyError:
-                # if either of the access keys aren't there, use local aws config
-                self.has_credentials = False
+                self.has_credentials = False # if neither access key is there, use local aws config
 
     def send_mass_email(self, **kwargs):
         # ses client params -- if they exist, pass directly to client constructor
@@ -32,12 +31,17 @@ class ses_mailer:
         subject = kwargs['subject']
         # html can contain tokens for replacement like this: {token_name}
         # 'token_name' must be a key in the recipient dicts below (r)
-        html = kwargs['html_text']
+        if kwargs['html_file']:
+            with open(kwargs['html_file'], 'rb') as f:
+                html = f.read()
+        else:
+            html = kwargs['html_text']
 
         # one-liners are fun -- if we have a filename, get recipients from there, else from kwargs
         recipient_data = self.get_recipients_from_csv(kwargs['csv']) if 'csv' in kwargs else kwargs['recipient_data']
 
         sent_emails = []
+        bad_emails = []
         for r in recipient_data:
             r['timestamp'] = datetime.datetime.now().__str__()
             response = client.send_email(
@@ -59,12 +63,17 @@ class ses_mailer:
                         )
             sent_emails.append(r)
             if response['ResponseMetadata']['HTTPStatusCode'] != 200:
-                # log the error somewhere
-                pass
+                bad_emails.append(r)
 
-    # def send_email(**kwargs):
+        if 'logfile' in kwargs:
+            with open(kwargs['logfile'], 'wb') as f:
+                writer = csv.DictWriter(f)
+                writer.writeheader()
+                for s in sent_emails:
+                    writer.writerow(s)
 
-    @classmethod
+        return sent_emails
+
     def get_recipients_from_csv(filename):
         with open(filename, 'rb') as f:
             reader = csv.DictReader(f)
